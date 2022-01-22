a6429941b531a6f93090898f60bfd57dc4586e8a
Yi Chen Hock <35310170+yichenhock@users.noreply.github.com>
2022-01-22 17:36:18 +0000

    add grid background and centred canvas

--- src/components/canvasComponent.js
+++ src/components/canvasComponent.js
@@ -12,7 +12,6 @@ function CanvasComponent(props){
 	};
 
 	const draw = (p5) => {
-        
         p5.background(255, 130, 20)
         p5.ellipse(100, 100, 100)
         p5.ellipse(300, 100, 100)
