32a774f10d608821dba593538fb1596db5ed7233
tatiana-s <ts790@cam.ac.uk>
2022-01-22 19:16:20 +0000

    some vector stuff

--- src/components/canvasComponent.js
+++ src/components/canvasComponent.js
@@ -1,32 +1,33 @@
 import React from "react";
 import Sketch from "react-p5";
+import Vector from "../drawVector"
+import { updateDelta } from "../drawVector"
 
-let x = 50;
-let y = 50;
+let currentPos = new Vector(0, 0);
+let currentDelta = new Vector(0, 1);
+let velocity = 100;
 
-function CanvasComponent(props){
+function CanvasComponent(props) {
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
 		p5.createCanvas(750, 500).parent(canvasParentRef);
+		p5.background(255, 255, 255);
 	};
 
 	const draw = (p5) => {
-        
-        p5.background(255, 130, 20)
-        p5.ellipse(100, 100, 100)
-        p5.ellipse(300, 100, 100)
-
-		// p5.background(0);
-		// p5.ellipse(x, y, 70, 70);
-
-		// NOTE: Do not use setState in the draw function or in functions that are executed
-		// in the draw function...
-		// please use normal variables or class properties for these purposes
-		x++;
+		p5.noFill();
+		p5.stroke(0);
+		console.log(JSON.stringify(currentDelta));
+		currentDelta = updateDelta("right", currentDelta);
+		let newX = currentPos.x + currentDelta.x * velocity;
+		let newY = currentPos.y + currentDelta.y * velocity;
+		console.log(newX);
+		console.log(newY);
+		p5.line(currentPos.x, currentPos.y, newX, newY);
 	};
 
-    return <Sketch setup={setup} draw={draw} />;
-}; 
+	return <Sketch setup={setup} draw={draw} />;
+};
 
 export default CanvasComponent; 
