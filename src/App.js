d3ab21df842f6ff6d299f1574b314801314a9169
Yi Chen Hock <35310170+yichenhock@users.noreply.github.com>
2023-12-01 12:47:38 +0800

    add direction indicator

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import { useState } from 'react';
 import { CommandContext } from './components/commandContext.js';
 import MainComponent from './components/mainComponent.js'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    fontFamily: [
+      'Comfortaa',
+    ].join(','),
+  },
+});
+
 function App() {
   const [command, setCommand] = useState(null);
   const [currColour, setCurrentColour] = useState([0,0,0]); 
   const [colourToMix, setColourToMix] = useState(['black',null]); // null, true (more), false (less)
 
   return (
-    <CommandContext.Provider value={{command, setCommand, currColour, setCurrentColour, colourToMix, setColourToMix}}>
-      <MainComponent/>
-    </CommandContext.Provider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <CommandContext.Provider value={{command, setCommand, currColour, setCurrentColour, colourToMix, setColourToMix}}>
+        <MainComponent/>
+      </CommandContext.Provider>
+    </ThemeProvider>
   );
 }
 
